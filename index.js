f442ad9f6097f10ca4a7fb15409c7d10bed31568
ak8459 <87300147+ak8459@users.noreply.github.com>
2023-12-04 17:31:06 +0530

    frontend

--- index.js
+++ index.js
@@ -2,15 +2,14 @@ const express = require('express');
 const { connection } = require('./db');
 
 require('dotenv').config()
-// const cors = require('cors');
+const cors = require('cors');
 const  UserRouter  = require('./routes/User')
 const app = express();
-// app.use(cors())
+app.use(cors())
 app.use(express.json());
 
 
 app.use('/contact', UserRouter);
-// app.use('/notes', noteRouter);
 
 app.listen(process.env.port, async () => {
     try {
